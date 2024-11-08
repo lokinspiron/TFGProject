cb1e9a247ca7b1a6bfd9e1151087d36354e62cb8
Leo <Leo@LOKINSPIRON05>
2024-11-08 14:01:47 +0100

    Icono&Nombre

---
+++ app/src/main/java/com/example/tfgproject/RegisterScreen.kt
@@ -0,0 +1,14 @@
+package com.example.tfgproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inventory.tfgproject.R
+
+class RegisterScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tfgproject/RegisterScreen2.kt
@@ -0,0 +1,12 @@
+package com.example.tfgproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inventory.tfgproject.R
+
+class RegisterScreen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_screen2)
+    }
+}
