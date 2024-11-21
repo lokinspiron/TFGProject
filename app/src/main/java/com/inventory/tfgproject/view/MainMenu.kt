63d300bf24a8cf965dfcde659b0fb5b6bd54946b
Leo <Leo@LOKINSPIRON05>
2024-11-21 23:59:41 +0100

    Edits

--- app/src/main/java/com/inventory/tfgproject/view/MainMenu.kt
+++ app/src/main/java/com/inventory/tfgproject/view/MainMenu.kt
@@ -1,5 +1,6 @@
 package com.inventory.tfgproject.view
 
+import android.content.ClipData.Item
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
@@ -9,12 +10,14 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 import com.inventory.tfgproject.R
 import com.inventory.tfgproject.databinding.ActivityMainMenuBinding
 
 class MainMenu : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
-    private lateinit var binding:ActivityMainMenuBinding
+    private lateinit var binding: ActivityMainMenuBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,35 +26,36 @@ class MainMenu : AppCompatActivity(),NavigationView.OnNavigationItemSelectedList
         supportActionBar?.hide()
 
         val btnDrawerToggle: ImageButton = findViewById(R.id.btnDrawerToggle)
-        val drawerlt : DrawerLayout = findViewById(R.id.drawerlt)
-        val lltInventory : LinearLayout = findViewById(R.id.lltInventory)
-        val lltProvider : LinearLayout = findViewById(R.id.lltProvider)
+        val drawerlt: DrawerLayout = findViewById(R.id.drawerlt)
 
-        initListeners(btnDrawerToggle,drawerlt,lltInventory,lltProvider)
+        initListeners(btnDrawerToggle, drawerlt)
 
     }
 
-    private fun initListeners(btnDrawerToggle: ImageButton,drawerlt: DrawerLayout,lltInventory: LinearLayout,lltProvider: LinearLayout) {
+    private fun initListeners(btnDrawerToggle: ImageButton, drawerlt: DrawerLayout) {
 
         btnDrawerToggle.setOnClickListener {
             drawerlt.openDrawer(GravityCompat.START)
         }
 
-        val fragment_inventory = Inventory_Fragment()
-        lltInventory.setOnClickListener{
-            val inventory_transaction= supportFragmentManager.beginTransaction()
-            inventory_transaction.replace(R.id.fcvInventory,fragment_inventory)
-            inventory_transaction.addToBackStack(null)
-            inventory_transaction.commit()
+
+        val btnNavigationView = findViewById<BottomNavigationView>(R.id.bnvMenu)
+        btnNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.btnInventory -> {
+                    replaceFragment(Inventory_Fragment())
+                    true
+                }
+
+                R.id.btnProviders -> {
+                    replaceFragment(ProviderFragment())
+                    true
+                }
+
+                else -> false
+            }
         }
 
-        val fragment_provider = ProviderFragment()
-        lltProvider.setOnClickListener{
-            val provider_transaction = supportFragmentManager.beginTransaction()
-            provider_transaction.replace(R.id.fcvProviders,fragment_provider)
-            provider_transaction.addToBackStack(null)
-            provider_transaction.commit()
-        }
 
     }
 
@@ -59,8 +63,9 @@ class MainMenu : AppCompatActivity(),NavigationView.OnNavigationItemSelectedList
         return true
     }
 
+
     override fun onBackPressed() {
-        val drawerlt : DrawerLayout = findViewById(R.id.drawerlt)
+        val drawerlt: DrawerLayout = findViewById(R.id.drawerlt)
         if (drawerlt.isDrawerOpen(GravityCompat.START)) {
             drawerlt.closeDrawer(GravityCompat.START)
         } else {
@@ -68,4 +73,11 @@ class MainMenu : AppCompatActivity(),NavigationView.OnNavigationItemSelectedList
         }
     }
 
+    private fun replaceFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fcvInventory, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
 }
